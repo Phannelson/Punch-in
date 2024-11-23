343996f85da52e871d0ffe1dd9f955ad092e09c5
phannelson <phannelson@yahoo.com>
2024-11-23 11:46:41 -0800

    successfully migrated webapp to django

--- punch_in_project/punch_in_app/views.py
+++ punch_in_project/punch_in_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Hello World!")
+def index(request):
+    return render(request, 'punch_in_app/index.html')
